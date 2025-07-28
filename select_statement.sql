4d66d5014d50fde647b995092401be9bfe1e498a
Jaswanth-2 <jaswanthramesh24@gmail.com>
2025-07-28 23:31:40 +0530

    Add files via upload

---
+++ select_statement.sql
@@ -0,0 +1,26 @@
+SELECT * FROM school_students.parent_details;
+
+
+SELECT * FROM school_students.student_demograpic;
+
+SELECT first_name,
+last_name,
+age,
+age+2
+FROM school_students.student_demograpic;
+
+#PEDMAS Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
+SELECT first_name,
+last_name,
+age,
+(age / 2) + age - 2 + 10 AS PEDMAS
+FROM school_students.student_demograpic;
+
+#UNIQUE VALUE 
+SELECT DISTINCT gender
+FROM school_students.student_demograpic;
+
+SELECT DISTINCT first_name,gender
+FROM school_students.student_demograpic;
+
+
