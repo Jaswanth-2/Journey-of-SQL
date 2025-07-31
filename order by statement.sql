57699e753f8ac6ccd35bcf9e95cad5527544bda8
Jaswanth-2 <jaswanthramesh24@gmail.com>
2025-07-31 23:03:51 +0530

    Add files via upload

---
+++ order by statement.sql
@@ -0,0 +1,16 @@
+SELECT * FROM school_students.student_demograpic;
+
+SELECT * FROM school_students.student_demograpic
+ORDER BY gender;
+
+SELECT * FROM school_students.student_demograpic
+ORDER BY gender,age,first_Name;
+
+SELECT * FROM school_students.student_demograpic
+ORDER BY gender DESC ,age DESC;
+
+SELECT * FROM school_students.student_demograpic
+ORDER BY age DESC,gender DESC ;
+
+SELECT * FROM school_students.student_demograpic
+ORDER BY 7,4;
